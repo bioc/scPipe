785e294efb3383074747b39509e8458b9272bd60
shians <registertonysu@gmail.com>
2023-10-11 13:07:46 +1100

    Added tests

--- src/test-str_utils.cpp
+++ src/test-str_utils.cpp
@@ -10,8 +10,11 @@ context("String memory functions") {
         
         Rcpp::Rcout << str << " " << strlen(str) << std::endl;
 
-        // unit tests for str_alloc
+        // check that string has been placed into allocated memory
+        expect_true(str != NULL);
         expect_true(strlen(str) == 10);
+        expect_true(str[0] == 'a');
+        expect_true(str[9] == 'j');
         expect_true(str[10] == '\0');
         
         expect_error(str_alloc(-1));
@@ -24,9 +27,13 @@ context("String memory functions") {
         std::strncpy(str, "abcdefghij", 10);
         str = str_realloc(str, 15);
         
-        // unit tests for str_realloc
-        expect_true(str[10] == '\0');
+        // check that string has been retained in reallocated memory
+        expect_true(str != NULL);
         expect_true(str[0] == 'a');
+        expect_true(str[9] == 'j');
+        expect_true(str[10] == '\0');
+        expect_true(strlen(str) == 10);
+
         expect_error(str_realloc(str, -1));
     }
     
@@ -35,10 +42,17 @@ context("String memory functions") {
         std::strncpy(str, "abcdefghij", 10);    
         str = str_shift(str, 5);
         
-        // unit tests for str_shift
+        // check that string has been shifted
+        expect_true(str != NULL);
         expect_true(strlen(str) == 15);
-        expect_true(str[15] == '\0');
         expect_true(str[5] == 'a');
+        expect_true(str[15] == '\0');
+
+        // check old data still present
+        expect_true(str[0] == 'a');
+        expect_true(str[4] == 'e');
+        
+
         expect_error(str_shift(str, -1));
     }
 }
